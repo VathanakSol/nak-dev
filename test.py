18a2bcfe8c5fc1f0dbcc7a91c75aa16faf4a2b39
kali <kali@kali>
2024-08-29 11:57:03 +0700

    class concept

--- test.py
+++ test.py
@@ -1,2 +1,31 @@
 print("Hello DevSecOps X CyberSecurity X Spring Microservice")
-print("This is test recording video")
+
+# class concept in python
+class Piglet:
+    def speak(self):
+        print("oink oink")
+
+class Person:
+    def __init__(self, name, age, subject):
+        self.name=name
+        self.age=age
+        self.subject=subject
+    def showinfo(self):
+        print("Student name is {}\nAge is {} years old\nSubject is {}".format(self.name, self.age, self.subject))
+
+student1 = Person("Sokny",16,"DevSecOps")
+student1.showinfo()
+student2 = Person("Makara",32,"DevSecOps")
+student2.showinfo()
+
+# different between sort and sorted built-in function in python
+list1 = [5,2,8,9,4]
+list2 = sorted(list1)
+print(list1)
+print(list2)
+
+
+
+
+
+
